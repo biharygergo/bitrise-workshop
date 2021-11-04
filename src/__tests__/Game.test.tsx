195d8783832d7f9c51a17ba3e25fb31c913a8c25
Gergely Bihary <gergely.bihary@prezi.com>
2021-11-04 19:49:38 +0100

    feat: Added tests

---
+++ src/__tests__/Game.test.tsx
@@ -0,0 +1,41 @@
+import { render } from "@testing-library/react-native";
+import React from "react";
+import { IQuiz } from "../interfaces";
+import { Game } from "../screens";
+import { IProps } from "../screens/Game";
+
+const mockProps: IProps = {
+  currentIndex: 0,
+  isLoading: false,
+  livea: 3,
+  questions: [],
+  handleAnswerSelected: jest.fn(),
+  onResetGame: jest.fn(),
+};
+
+describe("Game", () => {
+  it("should match snapshot", () => {
+    const renderedGame = render(<Game {...mockProps} />);
+    expect(renderedGame.toJSON()).toMatchSnapshot();
+  });
+
+  it("should match snapshot while loading", () => {
+    const renderedGame = render(
+      <Game {...{ ...mockProps, isLoading: true }} />
+    );
+    expect(renderedGame.toJSON()).toMatchSnapshot();
+  });
+
+  it("should match snapshot with questions", () => {
+    const question: IQuiz = {
+      question: "How are you?",
+      correct_answer: "Great",
+      incorrect_answers: ["Bad", "Meh", "Bleh"],
+      category: "feelings",
+      type: "multiple",
+      difficulty: "easy",
+    };
+    const renderedGame = render(<Game {...{ ...mockProps, questions: [question] }} />);
+    expect(renderedGame.toJSON()).toMatchSnapshot();
+  });
+});
